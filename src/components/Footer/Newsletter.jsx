618b610d5a4476f99feca1ec28f4b693b7fae037
George Udonte <georgeudonte@gmail.com>
2023-01-06 09:54:51 +0100

    restaurant app: completed all section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import SubHeading from '../SubHeading/SubHeading';
 
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
-
 const Newsletter = () => (
-  <div className='app__newsletter'>
-    <div className='app__newsletter-heading'>
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
       <p className="p__opensans">And never miss latest Updates!</p>
     </div>
     <div className="app__newsletter-input flex__center">
-    <input type='email' placeholder='Enter your email address' />
-    <button className='custom__button'></button>
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
   </div>
 );
