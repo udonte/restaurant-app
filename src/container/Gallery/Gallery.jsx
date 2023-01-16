ccbf6271731052132f9c35a6ae8748d1cbd8943e
George Udonte <georgeudonte@gmail.com>
2023-01-16 10:44:04 +0100

    added copy for gallery section

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -23,7 +23,7 @@ const Gallery = () => {
       <div className="app__gallery-content">
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Experience authentic international flavors in our stunning photo gallery. From exotic dishes to cultural decor, let us transport you on a culinary journey.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
       <div className="app__gallery-images">
